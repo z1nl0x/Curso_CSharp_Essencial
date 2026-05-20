53fe2e8954d89d8a06f37f6b100c4f30ab239b28
z1nl0x <paulo.kreft@gmail.com>
2026-05-20 17:52:23 -0300

    Iniciando as aulas: Entendendo arquivos e estruturas de um projeto C# e Iniciando aprendizado dos tipos de dados na linguagem

---
+++ CSFundamentos1/TiposDeDados1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+// declarando variáveis
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+uint valor4 = 2147483647;
+long valor5 = -21474836489;
+
+const int valor6 = 999;
+int var1 = 8, var2 = 9, var3 = 10;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+
+Console.ReadLine();
